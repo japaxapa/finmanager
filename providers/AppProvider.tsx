5a095b7be313d13eb58aab19ab4868e3ae016fd7
Mario Kawakita <mario.jhk@gmail.com>
2026-06-25 12:48:44 -0300

    feat: navbar

--- providers/AppProvider.tsx
+++ providers/AppProvider.tsx
@@ -5,12 +5,14 @@ import { QueryProvider } from './QueryProvider';
 import { ThemeProvider } from '@mui/material/styles';
 import { globalTheme } from '@/shared/lib/mui.theme';
 import GlobalToaster from '@/shared/lib/toaster';
+import AppNavBar from '@/shared/components/AppNavBar';
 
 export function AppProviders({ children }: PropsWithChildren) {
   return (
     <ThemeProvider theme={globalTheme}>
       <QueryProvider>
         <GlobalToaster />
+        <AppNavBar />
         {children}
       </QueryProvider>
     </ThemeProvider>

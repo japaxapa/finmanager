9c48408085476dd88da433a69235028a1ab96323
Mario Kawakita <mario.jhk@gmail.com>
2026-08-13 15:06:03 -0300

    feat: supabase type helper

---
+++ shared/lib/supabase/types/database.helpers.ts
@@ -0,0 +1,16 @@
+import { Database } from './supabase';
+
+// Helper to extract table rows
+export type Tables<T extends keyof Database['public']['Tables']> =
+  Database['public']['Tables'][T]['Row'];
+
+// Helper to extract table inserts
+export type TablesInsert<T extends keyof Database['public']['Tables']> =
+  Database['public']['Tables'][T]['Insert'];
+
+// Helper to extract table updates
+export type TablesUpdate<T extends keyof Database['public']['Tables']> =
+  Database['public']['Tables'][T]['Update'];
+
+// Helper to extract database Enums
+export type Enums<T extends keyof Database['public']['Enums']> = Database['public']['Enums'][T];

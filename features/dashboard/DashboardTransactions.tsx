db3810ea5d9198bc408e245506939dc85d34f4dd
Mario Kawakita <mario.jhk@gmail.com>
2026-09-03 16:51:16 -0300

    feat: dashboard transaction list

--- features/dashboard/DashboardTransactions.tsx
+++ features/dashboard/DashboardTransactions.tsx
@@ -1,11 +1,18 @@
-import { TransactionRow } from '@/shared/components/TransactionRow';
+'use client';
+
 import { Card, CardContent, Stack, Box, Typography, Button, List } from '@mui/material';
-import { RECENT_TRANSACTIONS } from './mock.data';
+import { useTransactions } from '@/shared/hooks/useTransactions';
+import { TransactionList } from '@/shared/components/TransactionListManager';
 
 export default function DashboardTransactions() {
   {
     /* 4. Recent Transactions Section */
   }
+
+  const { data: transactionsData } = useTransactions({
+    page: 1,
+  });
+
   return (
     <Card variant="outlined" sx={{ borderRadius: 3 }}>
       <CardContent sx={{ p: 3 }}>
@@ -31,9 +38,10 @@ export default function DashboardTransactions() {
 
         {/* Transactions List */}
         <List disablePadding>
-          {RECENT_TRANSACTIONS.map((transaction, idx) => (
-            <TransactionRow key={idx} {...transaction} />
-          ))}
+          <TransactionList
+            transactions={transactionsData?.data ?? []}
+            emptyMessage="Nenhuma transação recente."
+          />
         </List>
       </CardContent>
     </Card>
